eb0e89c18bbf84558d5554c3371b5234fc0c2fdb
gorkemzrnc <gorkemzurnaci@hotmail.com>
2023-12-12 22:14:21 +0300

    .

--- BirimTestOrnek1/src/main/java/tr/edu/medipol/yazilimort/Hesaplamalar.java
+++ BirimTestOrnek1/src/main/java/tr/edu/medipol/yazilimort/Hesaplamalar.java
@@ -1,5 +1,16 @@
 package tr.edu.medipol.yazilimort;
 
 public class Hesaplamalar {
-
+	public int faktoriyelRec(int n ) {
+		if (n == 1) return 1;
+		return n * faktoriyelRec(n-1);
+	}
+	
+	public int faktoriyelLoop(int n) {
+		int sonuc = 1;
+		for(int i = n; i>1; i--) {
+			sonuc = sonuc * 1;
+		}
+		return sonuc;
+	}
 }
